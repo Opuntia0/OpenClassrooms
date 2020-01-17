9fae1c07b915762ea232bfa0817fffcdded4d513
Mathilde <opuntia0@gmail.com>
2020-01-17 13:28:06 +0100

    test modif

--- Android/Entrevoisins/app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
+++ Android/Entrevoisins/app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
@@ -5,11 +5,11 @@ import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.UiThreadTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.controller.ProfileView;
-import com.openclassrooms.entrevoisins.model.Neighbour;
 import com.openclassrooms.entrevoisins.ui.neighbour_list.ListNeighbourActivity;
 import com.openclassrooms.entrevoisins.utils.DeleteViewAction;
 import com.openclassrooms.entrevoisins.utils.ItemListClicViewAction;
@@ -20,13 +20,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.action.ViewActions.swipeRight;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.hamcrest.core.IsNull.notNullValue;
 
 
@@ -59,7 +66,7 @@ public class NeighboursListTest {
     @Test
     public void myNeighboursList_shouldNotBeEmpty() {
         // First scroll to the position that needs to be matched and click on it.
-        onView(ViewMatchers.withId(R.id.list_neighbours))
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
                 .check(matches(hasMinimumChildCount(1)));
     }
 
@@ -69,12 +76,12 @@ public class NeighboursListTest {
     @Test
     public void myNeighboursList_deleteAction_shouldRemoveItem() {
         // Given : We remove the element at position 2
-        onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT));
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed())).check(withItemCount(ITEMS_COUNT));
         // When perform a click on a delete icon
-        onView(ViewMatchers.withId(R.id.list_neighbours))
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(1, new DeleteViewAction()));
         // Then : the number of element is 11
-        onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT-1));
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed())).check(withItemCount(ITEMS_COUNT-1));
     }
 
     /**
@@ -85,7 +92,7 @@ public class NeighboursListTest {
     public void myNeighboursList_clicAction_shouldOpenDetailScreen() {
         Intents.init();
         // Clic sur le bloc du voisin
-        onView(ViewMatchers.withId(R.id.list_neighbours))
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(1, new ItemListClicViewAction()));
         // Vérifier que l'activité est lancée
         intended(hasComponent(ProfileView.class.getName()));
@@ -98,7 +105,7 @@ public class NeighboursListTest {
     @Test
     public void myNeighboursList_textView_shouldShowCorrectName() {
         // Démarrer l'activité de visionnage de profil
-        onView(ViewMatchers.withId(R.id.list_neighbours))
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(1, new ItemListClicViewAction()));
         onView(ViewMatchers.withId(R.id.name)).check(matches(withText("Jack")));
     }
@@ -108,11 +115,57 @@ public class NeighboursListTest {
      * favoris.
      */
 
-    // à modifier
+
     @Test
     public void myNeighboursList_favouriteView_shouldShowOnlyFavouriteNeighbours() {
-        // on perform
-        // set fav
+        onView(withId(R.id.container)).perform(swipeRight());
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayingAtLeast(10))).check(withItemCount(12));
+
+
+        synchronized (this) {
+            // Jack devient favoris
+            onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
+                    .perform(RecyclerViewActions.actionOnItemAtPosition(1, new ItemListClicViewAction()));
+            onView(withId(R.id.fab_fav)).perform(click());
+            onView(withId(R.id.back_button)).perform(click());
+        }
+        synchronized (this) {
+            // Elodie devient favoris
+            onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
+                    .perform(RecyclerViewActions.actionOnItemAtPosition(3, new ItemListClicViewAction()));
+            onView(withId(R.id.fab_fav)).perform(click());
+            onView(withId(R.id.back_button)).perform(click());
+        }
+
+        synchronized (this) {
+            // Vincent devinet favoris
+            onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
+                    .perform(RecyclerViewActions.actionOnItemAtPosition(4, new ItemListClicViewAction()));
+            onView(withId(R.id.fab_fav)).perform(click());
+            onView(withId(R.id.back_button)).perform(click());
+        }
+
+
+        onView(withId(R.id.container)).perform(swipeLeft());
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayingAtLeast(10))).check(withItemCount(3));
+
+        // Jack dans l'onglet favoris
+        /*onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, new ItemListClicViewAction()));
+        onView(ViewMatchers.withId(R.id.name)).check(matches(withText("Jack")));
+        onView(withId(R.id.back_button)).perform(click());
+
+        // Vincent dans l'onglet favoris
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(1, new ItemListClicViewAction()));
+        onView(ViewMatchers.withId(R.id.name)).check(matches(withText("Vincent")));
+        onView(withId(R.id.back_button)).perform(click());
+
+        // Elodie dans l'onglet favoris
+        onView(allOf(ViewMatchers.withId(R.id.list_neighbours), isDisplayed()))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(2, new ItemListClicViewAction()));
+        onView(ViewMatchers.withId(R.id.name)).check(matches(withText("Elodie")));
+        onView(withId(R.id.back_button)).perform(click());*/
 
         //onView(ViewMatchers.withId(R.id.list_favorite)).check(withItemCount(ITEMS_FAV));
     }
